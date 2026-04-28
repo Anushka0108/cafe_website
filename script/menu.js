7a07f55a2a23eb345def1ab1795ff27b2255f130
AnushkaAbhilash <anushkaabhilash2@gmail.com>
2026-04-28 12:03:05 +0530

    before php

--- script/menu.js
+++ script/menu.js
@@ -8,3 +8,32 @@ function selectItem(name, price, image,type){
     localStorage.setItem("selectedItem",JSON.stringify(item));
     window.location.href="item.html";
 }
+
+function searchItem() {
+    let input = document.getElementById("searchInput").value.toLowerCase().trim();
+    let items = document.querySelectorAll(".item");
+    let categories = document.querySelectorAll(".menu-category");
+
+    items.forEach(item => {
+        let name = item.querySelector("h4").innerText.toLowerCase();
+
+        if (name.includes(input)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+
+    categories.forEach(cat => {
+        let visible = false;
+        let catItems = cat.querySelectorAll(".item");
+
+        catItems.forEach(item => {
+            if (item.style.display !== "none") {
+                visible = true;
+            }
+        });
+
+        cat.style.display = visible ? "flex" : "none";
+    });
+}
